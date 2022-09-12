f9138616e7720e41a91dc3eecb85e1801fcf5a8c
4books <joonwoo9192@gmail.com>
2022-09-12 13:28:29 +0900

    javascript and nodejs

--- lecture/speed_check.js
+++ lecture/speed_check.js
@@ -1,8 +1,10 @@
 console.time('speed');
 
-for (let i = 0; i < 1_000_000_000; i++) {
-//10억번 도는데 몇초나 걸릴까
+for (let i = 0; i < 10_000_000_000; i++) {
+//100억번 도는데 몇초나 걸릴까
 }
 console.timeEnd('speed');
 
 //m1 맥북 에어 speed: 482.906ms
+//i7 8700k speed: 8.592s
+//
